610e3cb4bb1661555d1ef80f367b2805288d33f3
Mayur Kulkarni <mayurkulkarni012@gmail.com>
2017-04-17 17:10:22 +0530

    Rudimentary ImageViewer completed ;)

---
+++ src/Models/NeuralNetwork.java
@@ -0,0 +1,10 @@
+package Models;
+
+/**
+ * @author Mayur Kulkarni <mayurkulkarni012@gmail.com>
+ */
+public class NeuralNetwork {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
--- src/Utils/ImageViewer.java
+++ src/Utils/ImageViewer.java
@@ -2,38 +2,61 @@ package Utils;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 /**
  * Created by Mayur Kulkarni on 4/16/2017.
  */
 public class ImageViewer {
-    public static void main(String[] args) {
-        //image dimension
-        int width = 520;
-        int height = 520;
-        //create buffered image object img
+    public static void main(String[] args) throws IOException {
+        String path = "C:\\Users\\kulkarni_my\\Desktop\\Coursera-Machine-Learning\\Files\\Exercise 3\\character_data.csv";
+        BufferedReader br = new BufferedReader(new FileReader(path));
+        for (int i = 0; i < 2430; i++) {
+            br.readLine();
+        }
+        String line = br.readLine();
+        String[] splitLine = line.split(",");
+        double[] arr = new double[400];
+        for (int i = 0; i < 400; i++) {
+            arr[i] = Double.parseDouble(splitLine[i]);
+        }
+        int x = 0;
+        int[][] nicearr = new int[20][20];
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++, x++) {
+                nicearr[i][j] = (int) Math.abs(arr[x] * 200);
+            }
+        }
+        for (int i = 0; i < 20; i++) {
+            for (int j = 0; j < 20; j++) {
+                System.out.println(nicearr[i][j]);
+            }
+        }
+        createImage(nicearr);
+    }
+
+    public static void createImage(int[][] arr) {
+        int width = 20;
+        int height = 20;
         BufferedImage img = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        //file object
         File f = null;
-        //create random image pixel by pixel
         for(int y = 0; y < height; y++){
             for(int x = 0; x < width; x++){
-                int a = (int)(Math.random()*256); //alpha
-                int r = (int)(Math.random()*256); //red
-                int g = (int)(Math.random()*256); //green
-                int b = (int)(Math.random()*256); //blue
-//                a = r = g = b = 255;
-
+                int a = 235; //alpha
+                int ans = arr[x][y];
+                int r, g, b = ans == 0 ? 0 : ans; //arr[x][y] < 0 ? ans + 20 : ans;
+                System.out.println(b);
+                r = g = b;
                 int p = (a<<24) | (r<<16) | (g<<8) | b; //pixel
-
                 img.setRGB(x, y, p);
             }
         }
         //write image
         try{
-            f = new File("Output.png");
+            f = new File("D:\\Output.png");
             ImageIO.write(img, "png", f);
         }catch(IOException e){
             System.out.println("Error: " + e);
